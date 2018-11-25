cdeb53bad8b4779acaf534dc9dc6a273e1c7e99a
Ric012 <44015606+Ric012@users.noreply.github.com>
2018-11-25 16:18:54 +0100

    Warenkorb (#43)
    * added Cart fnkt im Manager + Warenkorb-Html
    
    * added CatalogController
    
    * added part/composite fnkt, cart get mapping
    
    * fixed CartOrderManager, added Order, changed tables
    
    * fixed manager
    
    * Bootstrap entfernt
    
    * Bootstrap entfernt

---
+++ src/main/java/kickstart/order/OrderController.java
@@ -0,0 +1,76 @@
+package kickstart.order;
+
+
+
+import kickstart.articles.Composite;
+import kickstart.articles.Part;
+import org.salespointframework.order.Cart;
+import org.salespointframework.order.Order;
+import org.salespointframework.order.OrderManager;
+import org.salespointframework.order.OrderStatus;
+import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.web.LoggedIn;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+
+@Controller
+@SessionAttributes("cart")
+public class OrderController {
+
+	private final CartOrderManager cartOrderManager;
+	private final OrderManager<Order> orderManager;
+
+	OrderController(OrderManager<Order> orderManager){
+
+		Assert.notNull(orderManager, "OrderManager must not be null!");
+		this.orderManager = orderManager;
+		this.cartOrderManager = new CartOrderManager(orderManager);
+	}
+
+	@ModelAttribute("cart")
+	Cart initializeCart() {
+
+	return cartOrderManager.initializeCart();
+	}
+
+	@GetMapping("/cart")
+	String basket() {
+		return "cart";
+	}
+
+	@GetMapping("/order")
+	String orderview(Model model, Model modelcompleted, Model modelorders) {
+
+		modelorders.addAttribute("openorders", cartOrderManager.getOrderManager().findBy(OrderStatus.OPEN));
+		model.addAttribute("paidorders", cartOrderManager.getOrderManager().findBy(OrderStatus.PAID));
+		modelcompleted.addAttribute("completeorders", cartOrderManager.getOrderManager().findBy(OrderStatus.COMPLETED));
+
+		return "order";
+	}
+
+	@PostMapping("/cart_composite")
+	String addComposite (@RequestParam("article") Composite article, @RequestParam("count") int count, @ModelAttribute Cart cart){
+
+		return cartOrderManager.addComposite(article,count,cart);
+
+	}
+
+	@PostMapping("/cart_part")
+	String addPart (@RequestParam("part") Part part, @RequestParam("count") int count, @ModelAttribute Cart cart){
+
+		return cartOrderManager.addPart(part, count, cart);
+
+	}
+
+															//Optional<UserAccount>
+	@PostMapping("/addorder")
+	String newOrder(@ModelAttribute Cart cart, Model model, @LoggedIn UserAccount account){
+
+	return cartOrderManager.newOrder(cart, model, account);
+	}
+}
