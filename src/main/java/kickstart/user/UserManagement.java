d3d745fb699e5cda8966a1315bb19ec0e2642618
Daniel <daniel@technikamateur.de>
2018-12-11 16:17:54 +0100

    Mitarbeiter können Admins werden. Hurra!

--- src/main/java/kickstart/user/UserManagement.java
+++ src/main/java/kickstart/user/UserManagement.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
+import lombok.NonNull;
+
 
 @Service
 @Transactional
@@ -116,7 +118,7 @@ public User findUserById (long id) {
 		
 	}
 	
-	public void changeRole(User user, int type) {
+	public void changeRole(@NonNull User user, int type) {
 		UserAccount userAccount = user.getUserAccount();
 		if (type == 0) { // Kunde zum Mitarbeiter machen
 			userAccount.add(Role.of("ROLE_EMPLOYEE"));
@@ -127,6 +129,12 @@ public User findUserById (long id) {
 			userAccount.add(Role.of("ROLE_CUSTOMER"));
 			userAccount.remove(Role.of("ROLE_EMPLOYEE"));
 			user.setSalary(0);
+			return;
+		} else if (type == 2) { // Mitarbeiter zum Admin machen
+			userAccount.add(Role.of("ROLE_BOSS"));
+			return;
+		} else if (type == 3) { // Admin zum Mitarbeiter machen
+			userAccount.remove(Role.of("ROLE_BOSS"));
 			return;
 		} else {
 			throw new IllegalArgumentException("Parameter type has illegal value");
