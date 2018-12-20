dd14cd7cf178a061a71eab2b70800e09168f96d0
Daniel <daniel@technikamateur.de>
2018-12-20 19:06:21 +0100

    Fix

--- src/main/java/kickstart/user/OrderCollector.java
+++ src/main/java/kickstart/user/OrderCollector.java
@@ -1,11 +1,6 @@
 package kickstart.user;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.salespointframework.order.Order;
 import org.salespointframework.order.OrderManager;
-import org.salespointframework.useraccount.Role;
 import org.salespointframework.useraccount.UserAccount;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
@@ -23,14 +18,6 @@ public class OrderCollector {
 	}
 	
 	public Streamable<CustomerOrder> findOrder(UserAccount userAccount){
-		Iterable<CustomerOrder> orderList = orderManager.findBy(userAccount);
-		for (CustomerOrder order : orderList) {
-		    System.out.println(order.getDateCreated().toString());
-		    System.out.println(order.getTotalPrice().toString());
-		    if (order.isOpen()) {
-		    	System.out.println("HALLO");
-		    }
-		}
 		return orderManager.findBy(userAccount);
 	}
 }
