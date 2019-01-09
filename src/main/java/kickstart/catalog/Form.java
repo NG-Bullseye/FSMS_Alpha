1914be2baeeefae25b721470b89998b2f0998663
Daniel Güth <daniel.gueth@mailbox.tu-dresden.de>
2019-01-09 09:26:52 +0100

    Update src/main/java/kickstart/catalog/Form.java

--- src/main/java/kickstart/catalog/Form.java
+++ src/main/java/kickstart/catalog/Form.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class Form {
 	@NotEmpty(message = "Bitte geben Sie einen Namen für das Produkt ein.")
