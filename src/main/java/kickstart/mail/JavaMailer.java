c6a47bd964560f939646eb3c46464e7251690482
Daniel <daniel@technikamateur.de>
2019-01-09 21:36:31 +0100

    update javadoc

--- src/main/java/kickstart/mail/JavaMailer.java
+++ src/main/java/kickstart/mail/JavaMailer.java
@@ -12,48 +12,54 @@ import lombok.NonNull;
  */
 @Component
 public class JavaMailer {
-  
-    private final JavaMailSender emailSender;
-    
-    /**
-     * Creates a new {@link JavaMailer} with the given JavaMailSender.
-     * 
-     * @param emailSender must not be {@literal null}.
-     */
-    public JavaMailer (@NonNull JavaMailSender emailSender) {
-    	this.emailSender = emailSender;
-    }
-    
-    /**
-     * Creates a new SimpleMailMessage which will be transfered after registration.
-     * 
-     * @param email must not be {@literal null}. Contains mail address of recipient.
-     */
-    public void sendCustomerRegistrationMessage(@NonNull String email) {
-        SimpleMailMessage message = new SimpleMailMessage();
+
+	private final JavaMailSender emailSender;
+
+	/**
+	 * Creates a new {@link JavaMailer} with the given JavaMailSender.
+	 * 
+	 * @param emailSender must not be {@literal null}.
+	 */
+	public JavaMailer(@NonNull JavaMailSender emailSender) {
+		this.emailSender = emailSender;
+	}
+
+	/**
+	 * Creates a new SimpleMailMessage which will be transfered after registration.
+	 * 
+	 * @param email must not be {@literal null}. Contains mail address of recipient.
+	 */
+	public void sendCustomerRegistrationMessage(@NonNull String email) {
+		SimpleMailMessage message = new SimpleMailMessage();
 		message.setTo(email);
 		message.setSubject("Möbel-Hier Registrierung");
 		message.setText("Sie haben sich erfolgreich bei Möbel-Hier registriert!");
 		sendMail(message);
 		return;
-    }
-    
-    public void sendCustomerConfirmationMessage(@NonNull String email) {
-    	SimpleMailMessage message = new SimpleMailMessage();
+	}
+
+	/**
+	 * Creates a new SimpleMailMessage which will be transfered if order is ready
+	 * for collection.
+	 * 
+	 * @param email must not be {@literal null}. Contains mail address of recipient.
+	 */
+	public void sendCustomerConfirmationMessage(@NonNull String email) {
+		SimpleMailMessage message = new SimpleMailMessage();
 		message.setTo(email);
 		message.setSubject("Möbel-Hier Bestellung");
 		message.setText("Ihre Ware ist ab sofort abholbereit!");
 		sendMail(message);
 		return;
-    }
-    
-    /**
-     * Send away a previously created SimpleMailMessage.
-     * 
-     * @param message must not be {@literal null}. Contains a impleMailMessage.
-     */
-    private void sendMail (SimpleMailMessage message) {
-    	emailSender.send(message);
-    	return;
-    }
+	}
+
+	/**
+	 * Send away a previously created SimpleMailMessage.
+	 * 
+	 * @param message must not be {@literal null}. Contains a impleMailMessage.
+	 */
+	private void sendMail(SimpleMailMessage message) {
+		emailSender.send(message);
+		return;
+	}
 }
