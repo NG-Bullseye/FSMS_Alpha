b513579c9892e33889abff968041add683a97230
Computer <leonard.r.wecke@gmail.com>
2020-11-03 16:27:14 +0100

    Umstruckturierung von:
    dort wurde alles von einzelnen methoden buy craft sell und recieve craft send
    in commitEmployee und commitManager umstrukturiert
    
    undo wurde im employee angepasst mit der neuen commit methode
    
    Implementierung von InventoryItemActions in allen Managern
    
    ManagerView.html
    EmployeeView.html
    hier wurden die buttons entfernt und ein commit button hinzugefügt
    
    die iteration über inventory.all wurde im ManagerView.html auf InventoryItemActions umstrukturiert
    
    hier befindet sich ein error

---
+++ src/main/java/kickstart/Micellenious/InventoryItemAction.java
@@ -0,0 +1,33 @@
+package kickstart.Micellenious;
+
+import kickstart.Manager.AdministrationManager;
+import lombok.Getter;
+import lombok.Setter;
+import org.salespointframework.catalog.ProductIdentifier;
+import org.salespointframework.inventory.InventoryItemIdentifier;
+
+public class InventoryItemAction {
+	@Setter
+	@Getter
+	private ProductIdentifier pid;
+	@Setter
+	@Getter
+	private int amountForCraft;
+	@Setter
+	@Getter
+	private int amountForOut;
+	@Setter
+	@Getter
+	private int amountForIn;
+	@Setter
+	@Getter
+	private int amountForZerlegen;
+
+	public InventoryItemAction(ProductIdentifier pid, int amountForIn, int amountForCraft, int amountForOut ) {
+		this.pid = pid;
+		this.amountForCraft = amountForCraft;
+		this.amountForOut = amountForOut;
+		this.amountForIn = amountForIn;
+		this.amountForZerlegen=0;
+	}
+}
