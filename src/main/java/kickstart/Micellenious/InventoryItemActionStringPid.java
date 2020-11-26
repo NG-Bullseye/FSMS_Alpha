a5e30c132f9d0a2bc9685139e42e336147f29226
Computer <leonard.r.wecke@gmail.com>
2020-11-26 15:03:30 +0100

    fill InventoryItemActionStringPid nachträglich Fertig!

--- src/main/java/kickstart/Micellenious/InventoryItemActionStringPid.java
+++ src/main/java/kickstart/Micellenious/InventoryItemActionStringPid.java
@@ -1,4 +1,34 @@
 package kickstart.Micellenious;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.salespointframework.catalog.ProductIdentifier;
+
 public class InventoryItemActionStringPid {
+	@Setter
+	@Getter
+	private String pidString;
+	@Setter
+	@Getter
+	private int amountForCraft;
+	@Setter
+	@Getter
+	private int amountForOut;
+	@Setter
+	@Getter
+	private int amountForIn;
+	@Setter
+	@Getter
+	private int amountForZerlegen;
+
+	public InventoryItemActionStringPid() {
+	}
+
+	public InventoryItemActionStringPid(String pidString, int amountForIn, int amountForCraft, int amountForOut ) {
+		this.pidString = pidString;
+		this.amountForCraft = amountForCraft;
+		this.amountForOut = amountForOut;
+		this.amountForIn = amountForIn;
+		this.amountForZerlegen=0;
+	}
 }
